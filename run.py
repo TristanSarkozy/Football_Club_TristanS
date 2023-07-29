00e017890965e3fda93d0c6a2de62a8a8bba3686
Suciu Dan <suciu_t_dan@yahoo.com>
2023-07-29 18:44:13 +0000

    Add new characteristics to objects and main function to meet the requirements

--- run.py
+++ run.py
@@ -14,25 +14,22 @@ SHEET = GSPREAD_CLIENT.open('Football_Club_TristanS')
 
 
 class Player:
-    def __init__(self, name, age):
+    def __init__(self, name, age, goals_scored, games_played):
         """
-        Create a Player object with the coresponding attributes
+        Create a Player object with the corresponding attributes
         """
         self.name = name
         self.age = age
-        self.goals_scored = 0
-        self.games_played = 0
+        self.goals_scored = goals_scored
+        self.games_played = games_played
 
     def __str__(self):
         """
         Return the string containing player details
         """
         return f"{self.name}, Age: {self.age}, Goals Scored: {self.goals_scored}, Games Played: {self.games_played}\n"
-"""
-player1 = Player("Dan Suciu", 43)
 
-print(player1)
-"""
+
 class FootballClub:
     def __init__(self):
         """
@@ -40,39 +37,40 @@ class FootballClub:
         """
         self.players = []
 
-    def add_player(self, name, age):
+    def add_player(self, name, age, goals_scored, games_played):
         """
-        Add new players to the football club
-        Raise ValueError if conditions are not met
-        Print result to console
+        Add new players to the football club.
+        Raise ValueError if conditions are not met.
+        Print statements.
         """
-        if not name:
-            raise ValueError("Please provide a valid name!")
+        if not any(char.isdigit() for char in name):
+            try:
+                age = int(age)
+                if age < 18 or age > 50:
+                    raise ValueError("Age should be between 18 and 50!")
+            except ValueError:
+                raise ValueError("Age must be a number between 18 and 50!")
+        
+            try:
+                games_played = int(games_played)
+            except ValueError:
+                raise ValueError("Games played must be a number!")
 
-        try:
-            age = int(age)
-            if age < 18 or age > 50:
-                raise ValueError("Age should be between 18 and 50!")
-        except ValueError:
-            raise ValueError("Age must be a number between 18 and 50!")
+            try:
+                goals_scored = int(goals_scored)
+            except ValueError:
+                raise ValueError("Goals scored must be a number!")
 
-        # Check if a player with the same name and age already exists
-        for player in self.players:
-            if player.name == name and player.age == age:
-                raise ValueError(f"{name}, Age: {age} already exists in the club!")
-        player = Player(name, age)
-        self.players.append(player)
-        print(f"{name} has been added to the club!\n")
-    """
-    club = FootballClub()
+            # Check if a player with the same name and age already exists
+            for player in self.players:
+                if player.name == name and player.age == age:
+                    raise ValueError(f"{name}, Age: {age} already exists in the club!")
+            player = Player(name, age, goals_scored, games_played)
+            self.players.append(player)
+            print(f"{name} has been added to the club!\n")
+        else:
+            raise ValueError("Name should only contain letters and no digits!")
 
-    try:
-        club.add_player("Dan Suciu", 43)
-        club.add_player("Chuck Norris", 40)
-        club.add_player("Dan Suciu", 43)
-    except ValueError as e:
-        print(f"Error: {e}")
-    """
     def delete_player(self, name):
         """
         Delete a player from the football club
@@ -94,3 +92,77 @@ class FootballClub:
 
         for player in self.players:
             print(player)
+
+
+def main():
+    """
+    Function to run the Football Club Automation System.
+    Allow user to add players, delete players, list all players or exit the program.
+    """
+    football_club = FootballClub()
+
+    # Print the main menu 
+    print("<<<<< Football Club Automation System >>>>>\n")
+    print("1. Add Player")
+    print("2. Delete Player")
+    print("3. List All Players")
+    print("4. Exit\n")
+
+    while True:
+        choice = input("Enter your choice (1/2/3/4): ")
+
+        if choice == '1':
+            while True:
+                name = input("Enter player name: ")
+                if not any(char.isdigit() for char in name):
+                    break
+                else:
+                    print("Name should only contain letters and no digits!")
+
+            while True:
+                age = input("Enter player age: ")
+                try:
+                    age = int(age)
+                    if age < 18 or age > 50:
+                        raise ValueError("Age should be between 18 and 50!")
+                except ValueError as e:
+                    print(f"Error: {e}")
+                else:
+                    break
+
+            while True:
+                goals_scored = input("Enter number of goals: ")
+                try:
+                    goals_scored = int(goals_scored)
+                except ValueError:
+                    print("Goals scored must be an integer!")
+                else:
+                    break
+
+            while True:
+                games_played = input("Enter number of games played: ")
+                try:
+                    games_played = int(games_played)
+                except ValueError:
+                    print("Games played must be an integer!")
+                else:
+                    break
+
+            football_club.add_player(name, age, goals_scored, games_played)
+
+        elif choice == '2':
+            name = input("Enter player name to delete: ")
+            football_club.delete_player(name)
+        elif choice =='3':
+            print("List of all players: ")
+            football_club.list_all_players()
+        elif choice == '4':
+            print("Exiting the program..")
+            break
+        else:
+            print("Invalid choice. Please try again!")
+
+
+# Check if the script runs as the main
+if __name__ == "__main__":
+    main()
